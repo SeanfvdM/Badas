ae6c2015c3fbb92312f48f272bdced5c2f20c268
SeanfvdM <seanf.gsm@gmail.com>
2020-10-14 13:12:49 +0200

    added font selector demo [wip]

--- app/src/main/java/com/badas/badassolution/ui/placeholder/PlaceholderFragment.java
+++ app/src/main/java/com/badas/badassolution/ui/placeholder/PlaceholderFragment.java
@@ -1,30 +1,71 @@
 package com.badas.badassolution.ui.placeholder;
 
+import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.badas.badassolution.R;
+import com.badas.badasstyle.FontDownloader.Font;
+import com.badas.badasstyle.FontDownloader.FontDialogFragment;
+import com.badas.badasstyle.FontDownloader.FontDownloader;
+
+import java.util.List;
 
 public class PlaceholderFragment extends Fragment {
 
+    @SuppressLint("SetTextI18n")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        @SuppressWarnings("deprecation") PlaceholderViewModel placeholderViewModel = ViewModelProviders.of(this).get(PlaceholderViewModel.class);
         View root = inflater.inflate(R.layout.fragment_placeholder, container, false);
         final TextView textView = root.findViewById(R.id.text_placeholder);
-        placeholderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        textView.setText("This is a placeholder screen");
+
+        String apiKey = getString(R.string.fontKey);
+        new FontDownloader()
+                .setJsonLabels(FontDownloader.FontJsonLabels.GOOGLE_FONT_JSON_LABELS)
+                .requestListDownload(FontDownloader.ApiLinks.GOOGLE_FONTS_API_NO_KEY,
+                        apiKey,
+                        new FontDownloader.FontListDownloaderCallback() {
+                            @Override
+                            public void onFontsReceived(String result) {
+
+                            }
+
+                            @Override
+                            public void onFontsReceived(List<Font> fonts) {
+
+                            }
+
+                            @Override
+                            public void onFailed(Exception e) {
+
+                            }
+                        });
+
+        final FontDialogFragment fontDialogFragment = new FontDialogFragment(apiKey);
+        fontDialogFragment.showRelativeFontSize();
+        fontDialogFragment.setFontListener(new FontDialogFragment.FontListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onFontSelectedListener(com.koolio.library.Font lastSelectedFont, Typeface lastSelectedTypeface) {
+                Toast.makeText(requireContext(), lastSelectedFont.getFontFamily(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button = root.findViewById(R.id.font_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fontDialogFragment
+                        .show(getChildFragmentManager(), "Fonts");
             }
         });
         return root;
